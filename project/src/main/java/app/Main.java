c64b410fcdc32d5bc010532c64a1e07c307ad2ff
earnest ma <me@earne.link>
2023-10-30 11:56:22 -0400

    CA structure

---
+++ project/src/main/java/app/Main.java
@@ -0,0 +1,12 @@
+package app;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame application = new JFrame("Test");
+        application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+}
