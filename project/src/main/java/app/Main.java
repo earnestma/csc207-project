f86b57b49570fbc2de2e6bd2f849384ff00857af
Daniel12581 <danielc12581@gmail.com>
2023-11-13 11:23:01 -0500

    - Created the entities and their constructors based on the UML - Updated the UML diagram

--- project/src/main/java/app/Main.java
+++ project/src/main/java/app/Main.java
@@ -3,10 +3,15 @@ package app;
 import javax.swing.*;
 import java.awt.*;
 import java.io.IOException;
+import java.time.*;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame application = new JFrame("Test");
-        application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        /// JFrame application = new JFrame("Test");
+        /// application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        LocalDateTime now = java.time.LocalDateTime.now();
+        LocalDateTime future = java.time.LocalDateTime.of(2023, 12, 10, 4, 5);
+        System.out.println(future.compareTo(future));
+
     }
 }
