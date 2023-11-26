bc383c0b9d155a83c13a435b1baf7caf742d4dde
earnest ma <me@earne.link>
2023-11-25 21:55:08 -0500

    Entity getters, setters

--- project/src/main/java/entity/Task.java
+++ project/src/main/java/entity/Task.java
@@ -1,28 +1,95 @@
-package project.src.main.java.entity;
+package entity;
+
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Task {
     private String name;
-    private String dueDate;
-    private int priority; // Lower value = High priority
+    private int id;
+    private boolean hasDueDate;
+    private LocalDateTime dueDate;
+    private int priority; // Todoist: 1 (highest) - 4 (least, default)
     private boolean completed;
-    private List<Reminder> reminders; // Stores a list of reminders, with the most
+    public List<Reminder> reminders; // Stores a list of reminders, with the most
                                      // urgent at the front?
 
-    public Task(String name, String dueDate){
+    public Task(String name, int priority) {
         this.name = name;
+        this.hasDueDate = false;
+        this.completed = false;
+        this.reminders = new ArrayList<Reminder>();
+        this.priority = priority;
+    }
+
+    public Task(String name, int priority, int id) {
+        this.name = name;
+        this.id = id;
+        this.hasDueDate = false;
+        this.completed = false;
+        this.reminders = new ArrayList<Reminder>();
+        this.priority = priority;
+    }
+
+    public Task(String name, int priority, LocalDateTime dueDate) {
+        this.name = name;
+        this.hasDueDate = true;
         this.dueDate = dueDate;
         this.completed = false;
         this.reminders = new ArrayList<Reminder>();
+        this.priority = 4; // default/least value
+    }
+
+    public Task(String name, int priority, LocalDateTime dueDate, int id) {
+        this.name = name;
+        this.id = id;
+        this.hasDueDate = true;
+        this.dueDate = dueDate;
+        this.completed = false;
+        this.reminders = new ArrayList<Reminder>();
+        this.priority = 4; // default/least value
     }
 
     public String getName() {
         return name;
     }
 
-    public String getDueDate() {
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean hasDueDate() {
+        return this.hasDueDate;
+    }
+
+    public LocalDateTime getDueDate() {
+        // check using hasDueDate() first
         return dueDate;
+    }
+
+    public void setDueDate(LocalDateTime dueDate) {
+        this.hasDueDate = true; // always set to true if run
+        this.dueDate = dueDate;
+    }
+
+    public void removeDueDate() {
+        this.hasDueDate = false;
+        this.dueDate = null;
+    }
+
+    public int getPriority() {
+        return this.priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public boolean getCompleted() {
+        return this.completed;
+    }
+
+    public void setCompleted(boolean state) {
+        this.completed = true;
     }
 }
