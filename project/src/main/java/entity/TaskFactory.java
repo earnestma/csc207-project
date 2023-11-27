2685ff938748056d58d5b424ba3d5150813c771a
earnest ma <me@earne.link>
2023-11-27 11:35:18 -0500

    Entity getters, setters (#13)

--- project/src/main/java/entity/TaskFactory.java
+++ project/src/main/java/entity/TaskFactory.java
@@ -1,7 +1,9 @@
-package project.src.main.java.entity;
+package entity;
+
+import java.time.LocalDateTime;
 
 public class TaskFactory {
-    public Task create(String taskName, String dueDate) {
-        return new Task(taskName, dueDate);
+    public Task create(String taskName, int priority, LocalDateTime dueDate) {
+        return new Task(taskName, priority, dueDate);
     }
 }
--- project/src/main/java/entity/User.java
+++ project/src/main/java/entity/User.java
@@ -1,18 +1,25 @@
-package main.java.entity;
+package entity;
+
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Properties;
 
 public class User {
     private String name;
     final private String username;
     final private String password;
-    private Map projects; // projects has {project1 id : project1, project2 id : project2...}
+    public Map<Integer, Project> projects; // projects has {project1 id : project1, project2 id : project2...}
 
-    public User(String name, String username, String password){
+    public User(String name, String username, String password) {
         this.name = name;
         this.username = username;
         this.password = password;
         this.projects = new HashMap<Integer, Project>();
+    }
+
+    public User(String name, String username, String password, Map<Integer, Project> projects) {
+        this.name = name;
+        this.username = username;
+        this.password = password;
+        this.projects = projects;
     }
 }
