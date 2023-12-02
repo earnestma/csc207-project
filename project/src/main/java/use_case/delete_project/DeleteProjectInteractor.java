a33f22ee8055c8771d96651ebba9d2e9fec250e8
annikcwq <129215309+annikcwq@users.noreply.github.com>
2023-12-02 16:52:04 -0500

    Update DeleteProjectInteractor.java

--- project/src/main/java/use_case/delete_project/DeleteProjectInteractor.java
+++ project/src/main/java/use_case/delete_project/DeleteProjectInteractor.java
@@ -15,6 +15,7 @@ public class DeleteProjectInteractor implements DeleteProjectInputBoundary {
     }
 
     public void execute(DeleteProjectInputData deleteProjectInputData) {
+        try{
         ArrayList<Project> projectList = this.userDataAccessObject.listProjects();
 
         String projectName = deleteProjectInputData.getProjectName();
@@ -32,5 +33,8 @@ public class DeleteProjectInteractor implements DeleteProjectInputBoundary {
 
         DeleteProjectOutputData deleteProjectOutputData = new DeleteProjectOutputData(projectList, false);
         deleteProjectPresenter.prepareSuccessView(deleteProjectOutputData);
+        } catch (NullPointerException e) {
+            deleteProjectPresenter.prepareFailView("Project does not exist");
+        }
     }
 }
