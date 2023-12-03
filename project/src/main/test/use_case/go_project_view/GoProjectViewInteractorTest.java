410de2a826eda44fe6b8bd75eb4e365a86923ed3
Daniel12581 <87462799+Daniel12581@users.noreply.github.com>
2023-12-03 07:51:53 -0500

    Go home view test Completed (#59)
    * Early copy of uml diagram
    
    * - Created the entities and their constructors based on the UML
    - Updated the UML diagram
    
    * - Created the entities and their constructors based on the UML
    - Updated the UML diagram
    
    * Rough draft of check time use case
    
    * Fixed errors
    
    * Small changes to tasks
    
    * Committing all changes made
    
    * Created View Manager
    
    * Update project/src/main/java/use_case/check_remaining_time/CheckTimeOutputData.java
    
    Co-authored-by: earnest ma <me@earne.link>
    
    * Update classes.puml
    
    I think the dates should be in LocalDateTime, since just Date will probalby not be enough often (for example, if there is something due at 9pm). Also, I don't know if we need a creationDate instance attribute for Task.
    
    * Added a project view and its corresponding interfaces adapters. Need to test.
    
    * Created Home View View (and its dependent classes like HomeViewState).
    
    Also, updated Main to include the HomeViewView and ProjectView
    
    * - Created Use case for selecting the project.
    - Updated Task, User, Project and Main to include helper methods.
    - Fixed mistakes in ProjectView and HomeViewView
    
    * Also Updated these for SelectProject Use Case
    
    * Completed GoHomeView Use Case (To go back to home view from project view) and its corresponding components
    - Updated Main and ProjectView to accomodate the changes
    
    * Commit Small changes to CheckTime (Not Finished)
    
    * Merged and Fixed all issues until now.
    
    * Fixed merging mistake
    
    * Completed SelectTask Use Case and its corresponding components from CA
    - Note TaskView only displays a sample screen right now
    
    * Finished CheckTime use case and its corersponding components in CA.
    - Updated TaskView to accomodate CheckTime use case
    
    * Fixed small mistakes from merging
    
    * Fixed small mistakes from merging
    
    * Fixed bug for view not updating when adding task
    
    * Connected CheckTimePresenter to the TaskView so the checktime use case is in function now
    
    * Fixed Delete Task view not updating
    
    * Changed ){ to ) { for consistent formatting
    
    * GoHomeView Test Completed
    
    ---------
    
    Co-authored-by: earnest ma <me@earne.link>

--- project/src/main/test/use_case/go_project_view/GoProjectViewInteractorTest.java
+++ project/src/main/test/use_case/go_project_view/GoProjectViewInteractorTest.java
@@ -1,8 +1,6 @@
 package use_case.go_project_view;
 
 import junit.framework.TestCase;
-import use_case.select_add_task.SelectAddTaskInputBoundary;
-import use_case.select_add_task.SelectAddTaskInteractor;
 
 public class GoProjectViewInteractorTest extends TestCase {
 
