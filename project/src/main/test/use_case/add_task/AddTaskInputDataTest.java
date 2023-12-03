d594c385de02b7173700e68b08c79b0891599763
Daniel12581 <danielc12581@gmail.com>
2023-12-03 07:47:34 -0500

    Added Input Data and Output Data test to the classes already on main

---
+++ project/src/main/test/use_case/add_task/AddTaskInputDataTest.java
@@ -0,0 +1,35 @@
+package use_case.add_task;
+
+import org.junit.jupiter.api.BeforeEach;
+import entity.Project;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AddTaskInputDataTest {
+    AddTaskInputData data;
+    Project project;
+    
+    @BeforeEach
+    void init(){
+        project = new Project("project name");
+        data = new AddTaskInputData("name", "date", project);
+    }
+    
+    @Test
+    void getTaskName(){
+        assertEquals("name", data.getTaskName());
+    }
+    
+    @Test
+    void getDueDate(){
+        assertEquals("date", data.getDueDate());
+    }
+    
+    @Test
+    void getProject(){
+        assertEquals(project, data.getProject());
+    }
+    
+    
+}
---
+++ project/src/main/test/use_case/select_delete_task/SelectDeleteTaskOutputDataTest.java
@@ -0,0 +1,21 @@
+package use_case.select_delete_task;
+
+import entity.Project;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SelectDeleteTaskOutputDataTest {
+    SelectDeleteTaskOutputData data;
+    Project project;
+    @BeforeEach
+    void init(){
+        data = new SelectDeleteTaskOutputData(project);
+    }
+    
+    @Test
+    void getProject(){
+        assertEquals(data.getProject(), project);
+    }
+}
